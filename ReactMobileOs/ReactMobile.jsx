d2eaac9aae52b344b2db18a26580d19722c5c42e
Saurabh884 <99671592+Saurabh884@users.noreply.github.com>
2022-05-26 22:46:39 +0530

    Add files via upload

---
+++ ReactMobileOs/ReactMobile.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ReactMobile = () => {
+  return (
+      <>
+    <div>
+      <h1>Mobile Operating System</h1>
+      <ul>
+          <li>Android </li>
+          <li>Blackberry</li>
+          <li>Iphone</li>
+          <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+          <li>Samsung</li>
+          <li>HTC</li>
+          <li>MIcromax</li>
+          <li>Apple</li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default ReactMobile
